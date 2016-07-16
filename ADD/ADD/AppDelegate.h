7b6ae3590cb5f936e6c6edfa01f305d98de3bcab
你的名字 <Email>
2016-07-16 21:52:08 +0800

    版本1
    版本更新

---
+++ ADD/ADD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADD
+//
+//  Created by 潘自强 on 16/7/16.
+//  Copyright © 2016年 panziqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADD/ADD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ADD
+//
+//  Created by 潘自强 on 16/7/16.
+//  Copyright © 2016年 panziqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
